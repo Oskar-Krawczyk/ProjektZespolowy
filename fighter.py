8c5e1fb33c0d685b8950675d156ce82be6533795
Kamil <k.merc@op.pl>
2023-10-16 20:25:45 +0200

    Add additional comments

--- fighter.py
+++ fighter.py
@@ -7,8 +7,11 @@ class Fighter():
         pygame.draw.rect(surface,(255,0,0),self.rect)
         
     def move(self):
+        #Speed of character
         speed = 10
+        #To change X cordinates
         deltaX = 0
+        #To change Y cordinates
         deltaY = 0
         #Get what key is pressed
         keyPressed = pygame.key.get_pressed()
